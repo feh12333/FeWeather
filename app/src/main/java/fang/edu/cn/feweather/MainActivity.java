57913d4c238d0a76d1db0e33724c2d67cd8d0d80
feh12333 <914534439@qq.com>
2017-06-10 15:02:59 +0800

    First commit.

---
+++ app/src/main/java/fang/edu/cn/feweather/MainActivity.java
@@ -0,0 +1,13 @@
+package fang.edu.cn.feweather;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+    }
+}
